4f94424f72b1266ff6a54d282f31c7eefb20cdd7
asnider2 <angela_snider@brown.edu>
2024-04-03 02:29:17 -0400

    added tests

--- tests/test-example.r
+++ tests/test-example.r
@@ -3,3 +3,17 @@ test_that("This is an example test", {
   b <- 10
   expect_equal(a * b, 20)
 })
+
+test_that("Check estimated coefficient for 'gdpPercap'", {
+  expect_equal(unname(coef(model)["gdpPercap"]), 0.00076, tolerance = 1e-5)
+})
+
+test_that("Check if the number of observations in the model is equal to 1704", {
+  expect_equal(nobs(model), 1704)
+})
+
+test_that("Check data validation", {
+  expect_true(length(unique(paste(data$year, data$country))) == nrow(data))
+  expect_true(all(data$gdpPercap >= 0 & data$gdpPercap <= 1e6))
+  expect_true(all(data$continent %in% c("Asia", "Europe", "Africa", "Americas", "Oceania"))) # nolint
+})
